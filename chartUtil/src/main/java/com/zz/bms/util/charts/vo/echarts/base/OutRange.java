653982424809f2ea727474d8c5f94dc8eeb55002
zhonglihong <zhonglihong>
2019-01-07 09:54:27 +0800

    初始化项目

--- chartUtil/src/main/java/com/zz/bms/util/charts/vo/echarts/base/OutRange.java
+++
@@ -1,5 +0,0 @@
-package com.zz.bms.util.charts.vo.echarts.base;
-
-public class OutRange extends InRange {
-
-}
--- webUtil/src/main/java/com/zz/bms/util/web/BrowserType.java
+++
@@ -1,8 +0,0 @@
-package com.zz.bms.util.web;
-
-/**
- *
- */
-public enum BrowserType {
-	IE11,IE10,IE9,IE8,IE7,IE6,Firefox,Safari,Chrome,Opera,Camino,Gecko,Other
-}
